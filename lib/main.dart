f8028790e3f7c6fdd9f4a5d3e6a144e869da4083
pradord <pradord@rose-hulman.edu>
2025-10-24 10:38:56 -0400

    Fix build: Add missing providers import to main.dart
    Build was failing with:
    Error: The getter 'localeProvider' isn't defined
    
    Fixed by importing app/providers.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:alphagrit/app/app.dart';
+import 'package:alphagrit/app/providers.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
